0f40c07890fed133af67f5bac3bc027c20cb0fc8
ριтєя мαяχ <pedrohmarques@gmail.com>
2025-05-17 00:07:52 +0100

    ok

--- src/todo.txt.lib/Todo.cs
+++ src/todo.txt.lib/Todo.cs
@@ -2,26 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace todo.txt.lib
+namespace todo.txt.lib;
+
+public record Todo(
+    bool IsCompleted,
+    char? Priority,
+    DateTime? CompletedAt,
+    DateTime? CreatedAt,
+    string Description,
+    string[] Projects,
+    string[] Contexts,
+    IReadOnlyDictionary<string, string> Metadata)
 {
-    public record Todo(
-        bool IsCompleted,
-        char? Priority,
-        DateTime? CompletedAt,
-        DateTime? CreatedAt,
-        string Description,
-        string[] Projects,
-        string[] Contexts,
-        IReadOnlyDictionary<string, string> Metadata)
+    public override string ToString()
     {
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            if (IsCompleted) sb.Append("x ");
-            if (Priority.HasValue) sb.AppendFormat("({0}) ", Priority.Value);
-            if (CompletedAt.HasValue) sb.AppendFormat("{0:yyyy-MM-dd} ", CompletedAt.Value);
-            if (CreatedAt.HasValue) sb.AppendFormat("{0:yyyy-MM-dd} ", CreatedAt.Value);
-            return sb.Append(Description).ToString();
-        }
+        var sb = new StringBuilder();
+        if (IsCompleted) sb.Append("x ");
+        if (Priority.HasValue) sb.AppendFormat("({0}) ", Priority.Value);
+        if (CompletedAt.HasValue) sb.AppendFormat("{0:yyyy-MM-dd} ", CompletedAt.Value);
+        if (CreatedAt.HasValue) sb.AppendFormat("{0:yyyy-MM-dd} ", CreatedAt.Value);
+        return sb.Append(Description).ToString();
     }
 }
--- src/todo.txt.lib/TodoExtensions.cs
+++ src/todo.txt.lib/TodoExtensions.cs
@@ -1,46 +1,45 @@
 using System;
 using System.Globalization;
 
-namespace todo.txt.lib
+namespace todo.txt.lib;
+
+public enum Interval { d, w, m, y }
+public record Recurrence(DateTime Due, Interval Interval, int Period = 1)
 {
-    public enum Interval { d, w, m, y }
-    public record Recurrence(DateTime Due, Interval Interval, int Period = 1)
+    public Recurrence Next() => Interval switch
     {
-        public Recurrence Next() => Interval switch
-        {
-            Interval.d => new Recurrence(Due.AddDays(Period), Interval, Period),
-            Interval.w => new Recurrence(Due.AddDays(Period * 7), Interval, Period),
-            Interval.m => new Recurrence(Due.AddMonths(Period), Interval, Period),
-            Interval.y => new Recurrence(Due.AddYears(Period), Interval, Period),
-            _ => throw new NotImplementedException()
-        };
-    }
+        Interval.d => new Recurrence(Due.AddDays(Period), Interval, Period),
+        Interval.w => new Recurrence(Due.AddDays(Period * 7), Interval, Period),
+        Interval.m => new Recurrence(Due.AddMonths(Period), Interval, Period),
+        Interval.y => new Recurrence(Due.AddYears(Period), Interval, Period),
+        _ => throw new NotImplementedException()
+    };
+}
 
-    public static class TodoExtensions
+public static class TodoExtensions
+{
+    internal static DateTime? ParseDate(this string maybeDate)
+            => DateTime.TryParseExact(maybeDate, "yyyy-MM-dd", null, DateTimeStyles.None, out var d) ? d : null;
+
+    public static DateTime? Due(this Todo todo)
+        => todo.Metadata.TryGetValue("due", out var due) ? due.ParseDate() : null;
+
+    public static Recurrence Recurrence(this Todo todo)
     {
-        internal static DateTime? ParseDate(this string maybeDate)
-                => DateTime.TryParseExact(maybeDate, "yyyy-MM-dd", null, DateTimeStyles.None, out var d) ? d : null;
-
-        public static DateTime? Due(this Todo todo)
-            => todo.Metadata.TryGetValue("due", out var due) ? due.ParseDate() : null;
-
-        public static Recurrence Recurrence(this Todo todo)
+        // For a recurrence to exist, there must be a due date
+        if (todo.Due() is not DateTime due)
         {
-            // For a recurrence to exist, there must be a due date
-            if (todo.Due() is not DateTime due)
-            {
-                return null;
-            }
-
-            if (!todo.Metadata.TryGetValue("rec", out var recStr) ||
-                !Enum.TryParse<Interval>(recStr[^1..], out var rec) ||
-                !int.TryParse(recStr[..^1], out var period))
-            {
-                return null;
-            }
-
-            return new Recurrence(due, rec, period);
+            return null;
         }
 
+        if (!todo.Metadata.TryGetValue("rec", out var recStr) ||
+            !Enum.TryParse<Interval>(recStr[^1..], out var rec) ||
+            !int.TryParse(recStr[..^1], out var period))
+        {
+            return null;
+        }
+
+        return new Recurrence(due, rec, period);
     }
+
 }
