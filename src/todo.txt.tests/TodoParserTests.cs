5f67d2d3f0dba6acc97d106768ae0db9c0877730
ριтєя мαяχ <pedrohmarques@gmail.com>
2025-05-17 00:14:52 +0100

    ok

--- src/todo.txt.tests/TodoParserTests.cs
+++ src/todo.txt.tests/TodoParserTests.cs
@@ -7,50 +7,49 @@ using VerifyTests;
 using VerifyXunit;
 using Xunit;
 
-namespace todo.txt.tests
+namespace todo.txt.tests;
+
+[UsesVerify]
+public class TodoParserTests
 {
-    [UsesVerify]
-    public class TodoParserTests
+    private static VerifySettings Settings(Action<VerifySettings> configure = null)
     {
-        private static VerifySettings Settings(Action<VerifySettings> configure = null)
-        {
-            var s = new VerifySettings();
-            s.UseDirectory("snapshots");
-            s.DisableDiff();
-            s.ModifySerialization(_ => _.DontScrubDateTimes());
-            configure?.Invoke(s);
-            return s;
-        }
+        var s = new VerifySettings();
+        s.UseDirectory("snapshots");
+        s.DisableDiff();
+        s.ModifySerialization(_ => _.DontScrubDateTimes());
+        configure?.Invoke(s);
+        return s;
+    }
 
-        [Fact]
-        public Task TestRecurrence()
-        {
-            var todos = new TodoParser()
-                .ParseFile(@"data\recurrences.txt")
-                .Select(t => t.Recurrence())
-                .Select(r => new {rec=r, next=r?.Next()});
-            return Verifier.Verify(todos, Settings());
-        }
+    [Fact]
+    public Task TestRecurrence()
+    {
+        var todos = new TodoParser()
+            .ParseFile(@"data\recurrences.txt")
+            .Select(t => t.Recurrence())
+            .Select(r => new {rec=r, next=r?.Next()});
+        return Verifier.Verify(todos, Settings());
+    }
 
-        [Theory]
-        [InlineData(@"data\todo.txt")]
-        [InlineData(@"data\todo_large.txt")]
-        public Task TestFile(string file)
-        {
-            var todos = new TodoParser().ParseFile(file);
-            return Verifier.Verify(todos, Settings(s => s.UseParameters(file)));
-        }
+    [Theory]
+    [InlineData(@"data\todo.txt")]
+    [InlineData(@"data\todo_large.txt")]
+    public Task TestFile(string file)
+    {
+        var todos = new TodoParser().ParseFile(file);
+        return Verifier.Verify(todos, Settings(s => s.UseParameters(file)));
+    }
 
-        [Theory]
-        [InlineData(@"data\todo.txt")]
-        [InlineData(@"data\todo_large.txt")]
-        public void TestFileLines(string file)
+    [Theory]
+    [InlineData(@"data\todo.txt")]
+    [InlineData(@"data\todo_large.txt")]
+    public void TestFileLines(string file)
+    {
+        var parser = new TodoParser();
+        foreach (var line in File.ReadAllLines(file))
         {
-            var parser = new TodoParser();
-            foreach (var line in File.ReadAllLines(file))
-            {
-                Assert.Equal(line, parser.ParseLine(line).ToString());
-            }
+            Assert.Equal(line, parser.ParseLine(line).ToString());
         }
     }
 }
